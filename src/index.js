270af4546ab0b2a9b7a5890316514898d8bb220f
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-10 15:33:13 +0300

    basic router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const Dummy1 = () => {
+    return(
+        <div>Dummy1</div>
+    );
+}
+
+const Dummy2 = () => {
+    return(
+        <div>Dummy2</div>
+    );
+}
+
+const EditExpensePage = () => {
+    return(
+        <div>EditExpensePage</div>
+    );
+}
+
+const Help = () => {
+    return(
+        <div>Help</div>
+    );
+}
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route exact path='/' component={Dummy1}/>
+            <Route path='/create' component={Dummy2}/>
+            <Route path='/edit' component={EditExpensePage}/>
+            <Route path='/help' component={Help}/>
+        </div>
+    </BrowserRouter>
+);
+
+
+ReactDOM.render(routes, document.getElementById('root'));
+

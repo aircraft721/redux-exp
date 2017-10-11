e62f899dd2382485deed7488f981cd6e788d1fad
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-11 11:09:34 +0300

    added reducers with default data and created the store for the app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,62 @@ import './index.css';
 import App from './App';
 import AppRouter from './routers/AppRouter';
 
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+//expenses reducers
+
+const expensesReducerDefaultState = [];
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch(action.type){
+        default: 
+            return state;
+    }
+}
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+//store creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
+
+const demoState = {
+    expenses: [{
+        id: 'dasda',
+        description: 'January',
+        note: 'Final payment',
+        amount: 55000,
+        createdAt: 0
+    }],
+    filters: {
+        text: 'rent',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+    }
+};
+
+
+
+ReactDOM.render(<Provider store={store}><AppRouter /></Provider>, document.getElementById('root'));
 
  

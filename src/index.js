a5b8e0cb6feba08dafb84c2b843dd15be8c45b09
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-12 10:37:45 +0300

    hoc 2

--- src/index.js
+++ src/index.js
@@ -1,68 +1,31 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import AppRouter from './routers/AppRouter';
-// import configureStore from './store/configureStore';
-
-// import {Provider} from 'react-redux';
-// import {createStore, combineReducers} from 'redux';
-// import {addExpense} from './actions/expenses';
-// import {setTextFilter} from './actions/filters';
-// import getVisibleExpenses from './selectors/expenses';
-
-// const store = configureStore();
-
-// store.dispatch(addExpense({description: 'Water Bill'}))
-// store.dispatch(addExpense({description: 'Gas Bill'}))
-// store.dispatch(setTextFilter('water'))
-
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses)
-
-
-// console.log(store.getState());
-
-
-
-
-
-
-
-
-
-// ReactDOM.render(<AppRouter />, document.getElementById('root'));
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 
-const Info = (props) => {
-    return (
-        <div>
-            <h1>Info</h1>
-            <p>The info is: {props.info}</p>
-        </div>
-    );
-}
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
-const withAdminWarning = (WrappedComponent) => {
-    return (props) => {
-        return (
-            <div>
-                <p>This is private info. Please don't share!</p>
-                <WrappedComponent {...props}/>
-            </div>
-        );
-        
-    }
-}
+const store = configureStore();
 
-const AdminInfo = withAdminWarning(Info);
+store.dispatch(addExpense({description: 'Water Bill'}))
+store.dispatch(addExpense({description: 'Gas Bill'}))
+store.dispatch(setTextFilter('water'))
 
 
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses)
 
-ReactDOM.render(<AdminInfo info="There are the details" />, document.getElementById('root'));
+
+console.log(store.getState());
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
+
+
+
--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Info = () => {
+const Info = (props) => {
     return (
         <div>
             <h1>Info</h1>
@@ -10,4 +10,34 @@ const Info = () => {
     );
 }
 
-ReactDOM.render(<Info info='There are the details'/>, document.getElementById('root'));
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => {
+        return (
+            <div>
+                {props.isAdmin && <p>This is private info. Please don't share!</p>}
+                <WrappedComponent {...props}/>
+            </div>
+        );
+        
+    }
+}
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => {
+        return (
+            <div>
+                {props.isAuthenticated && <strong>dasdasdsa</strong>}
+                <WrappedComponent {...props} />
+            </div>
+        )
+    }
+}
+
+const AdminInfo = withAdminWarning(Info);
+
+const AuthInfo = requireAuthentication(Info);
+
+
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="There are the details" />, document.getElementById('root'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="There are the details" />, document.getElementById('root'));
